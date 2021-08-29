e80a1d1b088a4bfce2462482317df1198e781314
Muhammad Rendra <muhammadnurrendra@gmail.com>
2021-08-29 14:21:54 +0700

    2nd commit

---
+++ src/@materialios/index.js
@@ -0,0 +1,5 @@
+import hooks from './hooks'
+
+export {
+  hooks
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useContext, useEffect } from 'react'
 
-function App() {
+import {
+  ResponsiveContext,
+  DrawerContext
+} from '@materialios/contexts'
+
+import { MenuIcon } from '@materialios/icons'
+
+import {
+  MainScreen,
+  AppBar,
+  AppBarLeftIcon,
+  AppBarTitle,
+  Drawer,
+  DrawerNav,
+  DrawerNavExpandable
+} from '@materialios/components'
+
+import './App.css'
+
+const App = () => {
+  const { orientation, device } = useContext(ResponsiveContext)
+  const [drawer, setDrawer] = useContext(DrawerContext)
+
+  const [title, setTitle] = useState('Home')
+  const [isDesktopLandscape, setIsDesktopLandscape] = useState(false)
+
+  useEffect(() => {
+    setIsDesktopLandscape(orientation === 'Landscape' && device === 'Desktop')
+  }, [orientation, device])
+
+  useEffect(() => {
+    // console.log('drawer', drawer)
+  }, [drawer])
+
+  const handleNav = ({ name = '' }) => {
+    setTitle(name)
+    setDrawer({ show: false })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Drawer>
+        <DrawerNav name='Home' href='/' onClick={handleNav} />
+        <DrawerNavExpandable name='Products'>
+          <DrawerNav name='Alfa' href='/products/alfa' onClick={handleNav} />
+          <DrawerNav name='Bravo' href='/products/bravo' onClick={handleNav} />
+        </DrawerNavExpandable>
+        <DrawerNavExpandable name='Services'>
+          <DrawerNav name='One' href='/services/one' onClick={handleNav} />
+          <DrawerNav name='Two' href='/services/two' onClick={handleNav} />
+          <DrawerNav name='Three' href='/services/three' onClick={handleNav} />
+        </DrawerNavExpandable>
+      </Drawer>
+      <MainScreen>
+        <AppBar>
+          {!isDesktopLandscape && <AppBarLeftIcon icon={MenuIcon} onClick={() => setDrawer({ show: true })} />}
+          <AppBarTitle title={title} />
+        </AppBar>
+      </MainScreen>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
